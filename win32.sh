f06cff3498a60079aaf3d577ecd1cc3b69127f23
Tab Fitts <tfitts1212@gmail.com>
2018-03-30 12:56:17 -0700

    Add win32 build script

---
+++ win32.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+VERSION=2.5.0
+SRCPATH=$(pwd)
+git submodule --init
+git submodule --update
+cd admin/win/docker
+docker build . -t sprycloud-client-win32:$VERSION
+cd $SRCPATH
+docker run -v "$PWD:/home/user/client" sprycloud-client-win32:$VERSION    \
+        /home/user/client/admin/win/docker/build.sh client/  $(id -u)
