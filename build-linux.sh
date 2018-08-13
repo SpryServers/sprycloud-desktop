cd27f904dbf0f315dee4cdc4b0a19745e3085d3e
Tab Fitts <tfitts1212@gmail.com>
2018-08-12 22:31:39 -0700

    Add Linux build script
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

---
+++ build-linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SRCDIR=$(pwd)
+rm -rf build;
+rm -rf client-build;
+mkdir build
+mkdir client-build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr \
+      -DCMAKE_BUILD_TYPE="Release" \
+      -DNO_SHIBBOLETH=1 \
+      -DWITH_DOC=FALSE \
+      -DQTKEYCHAIN_LIBRARY=/usr/lib/libqt5keychain.so \
+      -DQTKEYCHAIN_INCLUDE_DIR=/usr/include/qt5keychain/
+make -j4
+make DESTDIR ../client-build install
+cd $SRCDIR
+cd client-build
+sed -i -e 's|Icon=sprycloud|Icon=spryCloud|g' usr/share/applications/sprycloud.desktop
+cd $SRCDIR
+echo 'Client successfully built. Client files at $SRCDIR/client-build'
